6d756398c9465755d250a0706d1030a18e3c7f6c
Eugenia Musat <musat.eugenia@gmail.com>
2021-09-09 14:17:34 +0200

    Add categories dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -4,8 +4,17 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-    $('.collapsible').collapsible()
-    $('.tooltipped').tooltip();
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $('select').formSelect();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
 
 
@@ -19,4 +28,17 @@ $(document).ready(function () {
 // document.addEventListener('DOMContentLoaded', function () {
 //     let sidenavs = document.querySelectorAll(".sidenav");
 //     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+//     let collapsibles = document.querySelectorAll(".collapsible");
+//     let collapsiblesInstance = M.Collapsible.init(collapsibles);
+//     let tooltips = document.querySelectorAll(".tooltipped");
+//     let tooltipsInstance = M.Tooltip.init(tooltips);
+//     let datepickers = document.querySelectorAll(".datepicker");
+//     let datepickersInstance = M.Datepicker.init(datepickers, {
+//         format: "dd mmmm, yyyy",
+//         yearRange: 3,
+//         showClearBtn: true,
+//         i18n: {
+//             done: "Select"
+//         }
+//     });
 // });
