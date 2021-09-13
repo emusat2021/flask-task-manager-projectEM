fcb6947b7d129646d22353500ed6d9b8c5ba4e84
Eugenia Musat <musat.eugenia@gmail.com>
2021-09-13 14:02:02 +0200

    Delete task from the database

--- static/js/script.js
+++ static/js/script.js
@@ -24,16 +24,21 @@ $(document).ready(function () {
             $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
         }
         $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            console.log("focusin");
             $(this).parent(".select-wrapper").on("change", function () {
+                console.log("change");
                 if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                    console.log("click1");
                     $(this).children("input").css(classValid);
                 }
             });
         }).on("click", function () {
+            console.log("click2");
             if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
                 $(this).parent(".select-wrapper").children("input").css(classValid);
             } else {
                 $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    console.log("focusout");
                     if ($(this).parent(".select-wrapper").children("select").prop("required")) {
                         if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
                             $(this).parent(".select-wrapper").children("input").css(classInvalid);
